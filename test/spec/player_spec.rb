11c4aae2e2c8a824515139d863c39793aaaf7eb4
Shaun Dern <smdern@gmail.com>
2011-10-28 20:37:21 -0700

    created player creation spec… todo

---
+++ test/spec/player_spec.rb
@@ -0,0 +1,11 @@
+require 'player'
+
+describe Player, "player creation" do
+  let(:name) { "Tetto" }
+  let(:race) { "Giant" }
+  let(:sex) { "Male"}
+
+  it "should not be able to create a player with a invalid name" do
+    pending "write a test for player creation"
+  end
+end
